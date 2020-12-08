cab242f596360be10eecb17663cc140ec25d8045
vega <1285511377@qq.com>
2020-12-08 20:41:30 +0800

    节点类

--- src/leetcode/base/ListNode.java
+++ src/leetcode/base/ListNode.java
@@ -1,12 +1,22 @@
 package leetcode.base;
 
 /**
+ * 节点类
  * @author vega
  */
 public class ListNode {
     public int val;
     public ListNode next;
-    public ListNode() {}
-    public ListNode(int val) { this.val = val; }
-    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
